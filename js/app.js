156a7cd864e9ef9677886597b08a3344a3a9f69d
tomascurcio <tomicurcio04@gmail.com>
2023-11-07 23:23:36 -0300

    primer version

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log("Conectado");
+
+for (let i = 0; i < 5; i++) {
+    console.log("Iteración número " + i);
+}
+
+let contador = 0;
+
+do {
+    console.log("Iteración número " + contador);
+    contador++;
+} while (contador < 5);
